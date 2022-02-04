d5cd8e353938bbb9340df30eb0351cd142501c41
AlexanderChck <92948608+AlexanderChck@users.noreply.github.com>
2022-02-04 15:47:58 +0300

    Add files via upload

--- Geocode.py
+++ Geocode.py
@@ -3,6 +3,7 @@ import requests
 
 def geocode(geocode):
     geocode = '+'.join(geocode.split())
+    print(geocode)
     geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={geocode}&format=json"
     response = requests.get(geocoder_request)
     return response
